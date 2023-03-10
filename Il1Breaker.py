3ba2376d3d4f908a64ab1cd35e062c444e7b3935
Michael Mandich <85703638+michaelmandich@users.noreply.github.com>
2023-03-10 15:56:48 -0500

    Create Il1Breaker.py
    This code is a simple script to hurt users who intentionally use deceptive usernames to evade cheating.

---
+++ Il1Breaker.py
@@ -0,0 +1,8 @@
+#input
+string = input("Enter a string of maximum length 16: ")
+
+#output
+string = string.replace('l', 'L').replace('I', 'i').replace('1', 'one')
+print(string)
+
+#run code and then paste in offending username.
